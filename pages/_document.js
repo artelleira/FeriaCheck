b2f44258c1f5e163fadb096fefb75e5c5c580874
artelleira <arteltestpc@gmail.com>
2026-03-19 10:44:20 -0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="es">
       <Head>
         <script src="https://cdn.tailwindcss.com"></script>
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDJZDl0JEFrM1IjJgczZ8mPY8L8L5jZZjA&libraries=places"></script>
       </Head>
       <body>
         <Main />
